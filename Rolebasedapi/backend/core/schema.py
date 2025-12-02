f8324b87370c66830efd90e0b1feb2510ebf75bc
aayushitechprobroker <aayushi.tech.probroker@gmail.com>
2025-12-02 22:49:26 +0530

    Add files via upload

---
+++ Rolebasedapi/backend/core/schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class Login(BaseModel):
+    email: str
+    password: str
+
+class LinkCreate(BaseModel):
+    link: str
